3b2377d9167c741ad2ff75578c835265999ee1b0
letquit <1446444589@qq.com>
2026-04-08 15:15:27 +0800

    Initial commit

---
+++ Assets/Scripts/GridObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Match3
+{
+    public class GridObject<T>
+    {
+        private GridSystem2D<GridObject<T>> grid;
+        private int x;
+        private int y;
+
+        public GridObject(GridSystem2D<GridObject<T>> grid, int x, int y)
+        {
+            this.grid = grid;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ Assets/Scripts/Match3.cs
@@ -0,0 +1,21 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Match3
+{
+    public class Match3 : MonoBehaviour
+    {
+        [SerializeField] private int width = 8;
+        [SerializeField] private int height = 8;
+        [SerializeField] private float cellSize = 1f;
+        [SerializeField] private Vector3 originPosition = Vector3.zero;
+        [SerializeField] private bool debug = true;
+
+        private GridSystem2D<GridObject<Gem>> grid;
+
+        private void Start()
+        {
+            grid = GridSystem2D<GridObject<Gem>>.HorizontalGrid(width, height, cellSize, originPosition, debug);
+        }
+    }
+}
